4b71b88ceae25994620dad005f36a772e37a71dc
Samarth Parekh <parekh.samarth@gmail.com>
2024-09-14 14:42:49 -0400

    adding more stuff

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const cors = require('cors');
+
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+
+const Transaction = require('./models/Transaction');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/api/test', (req, res) => {
+    res.json("Hello from the server!");
+});
+
+app.post('/api/transaction', async (req, res) => {
+    await mongoose.connect(process.env.MONGO_URL)
+    const {name, description, datetime, price} = req.body;
+    const transaction = await Transaction.create({name, description, datetime, price});
+    res.json(transaction);
+});
+
+app.listen(4040);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [datetime, setDatetime] = useState('');
+  const [description, setDescription] = useState('');
+
+  function addNewTransaction(e) {
+    e.preventDefault();
+    const url = process.env.REACT_APP_API_URL + '/transaction';
+
+    const price = name.split(' ')[0];
+
+
+    fetch(url, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        price,
+        name: name.substring(price.length + 1),
+        datetime,
+        description})
+    }).then(res => {
+      res.json().then(json => {
+        setName('');
+        setDatetime('');
+        setDescription('');
+        console.log('result',json);
+      });
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form onSubmit={addNewTransaction}>
+        <div className='basics'>
+          <input type="text" 
+                 value={name}
+                 onChange={e => setName(e.target.value)}
+                 placeholder={"+200 TV"} />
+          <input type="datetime-local"
+                 value={datetime}
+                 onChange={e => setDatetime(e.target.value)} />
+        </div>
+        <div className='description'>
+          <input type="text" 
+                 value={description}
+                 onChange={e => setDescription(e.target.value)}
+                 placeholder={"Description"} />
+        </div>
+        <button type="submit">Add Transaction</button>
+      </form>
+
+      <div className='transactions'>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>New TV</div>
+            <div className='description'>Needed new TV</div>
+          </div>
+          <div className='right'>
+            <div className='price red'>-$200</div>
+            <div className='datetime'>2024-01-01 12:00</div>
+          </div>
+        </div>
+
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>Salary</div>
+            <div className='description'>Needed new TV</div>
+          </div>
+          <div className='right'>
+            <div className='price green'>+$700</div>
+            <div className='datetime'>2024-01-01 12:00</div>
+          </div>
+        </div>
+
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name red'>New Shoes</div>
+            <div className='description'>Needed new TV</div>
+          </div>
+          <div className='right'>
+            <div className='price red'>-$200</div>
+            <div className='datetime'>2024-01-01 12:00</div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
